6521794f78102a4a7b0a4e198dc1fb25c5dd02bc
Trip4077 <bj2336@email.vccs.edu>
2019-03-10 13:15:53 -0400

    Basic state created and data fetched

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: 'avengers',
+      plot: 'short',
+      page: 2,
+      resuts: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://www.omdbapi.com/?s=${this.state.search}&plot=${this.state.plot}&page=${this.state.page}&apikey=db6f6716`)
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.log(err);
+          })
+  }
+
   render() {
     return (
       <div className="App">
