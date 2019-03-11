cd6b81308e369f993084594cb18db8e1b6514ceb
Trip4077 <bj2336@email.vccs.edu>
2019-03-10 23:34:34 -0400

    Basic search bar implemented

--- src/App.js
+++ src/App.js
@@ -7,27 +7,51 @@ class App extends Component {
     super(props);
 
     this.state = {
-      search: 'avengers',
+      search: '',
       plot: 'short',
-      page: 2,
-      resuts: []
+      page: 1,
+      totalMovies: 0,
+      results: []
     }
   }
 
-  componentDidMount() {
-    axios.get(`http://www.omdbapi.com/?s=${this.state.search}&plot=${this.state.plot}&page=${this.state.page}&apikey=db6f6716`)
+  handleChange = e => {
+    e.preventDefault();
+
+    const searchTerm = e.target.value;
+    this.performSearch(searchTerm);
+  }
+
+  performSearch = searchTerm => {
+    axios.get(`http://www.omdbapi.com/?s=${searchTerm}&plot=${this.state.plot}&page=${this.state.page}&apikey=db6f6716`)
           .then(res => {
-            console.log(res);
+            console.log(res.data);
+            this.setState({
+              results: res.data.Search,
+              totalMovies: Number(res.data.totalResults),
+              search: searchTerm
+            })
           })
           .catch(err => {
             console.log(err);
           })
   }
 
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App">
+        <form>
+          <input type='text'
+                 name='search'
+                 value={this.state.search}
+                 onChange={this.handleChange}
+                 placeholder='Enter movie title...' />
 
+        </form>
       </div>
     );
   }
