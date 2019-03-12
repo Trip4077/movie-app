865575c1ae60b6698a80359932d7f501fd5d43b2
Trip4077 <bj2336@email.vccs.edu>
2019-03-12 10:02:46 -0400

    Base Pagination Complete

--- src/App.js
+++ src/App.js
@@ -24,52 +24,51 @@ class App extends Component {
     this.performSearch(e, Number(e.target.innerText))
   }
 
-  incrementPage = () => {
-    this.setState(prevState => {return { page: prevState.page + 1 }});
+  incrementPage = e => {
+    this.performSearch(e, (this.state.page + 1))
   }
 
-  decrementPage = () => {
-    this.setState(prevState => {return { page: prevState.page + 1 }});
+  decrementPage = e => {
+    this.performSearch(e, (this.state.page - 1))
   }
 
   performSearch = (e, newpage) => {
     e.preventDefault();
 
     const page = newpage ? newpage : this.state.page;
-    console.log(page)
+    const lastPage = this.state.pageList[this.state.pageList.length-1]
     const searchTerm = this.state.search;
 
+    if (page > lastPage || page < 1) return;
     
-          axios.get(`http://www.omdbapi.com/?s=${searchTerm}&plot=${this.state.plot}&page=${page}&apikey=db6f6716`, {
-            params: {
-              foo: 'bar'
-            }
-          })
-          .then(res => {
-            const pageListEnd = res.data.totalResults % 10 === 0 ? Math.floor(res.data.totalResults / 10)
-                                                            : Math.floor(res.data.totalResults / 10) + 1;
+    axios.get(`http://www.omdbapi.com/?s=${searchTerm}&plot=${this.state.plot}&page=${page}&apikey=db6f6716`, {
+      params: { newpage: page }
+    })
+    .then(res => {
+      const pageListEnd = res.data.totalResults % 10 === 0 ? Math.floor(res.data.totalResults / 10)
+                                                      : Math.floor(res.data.totalResults / 10) + 1;
 
-            let list = [];
-            console.log(params.newpage)
-            for(let i = 1; i <= pageListEnd; i++ ) {
-              if(i > Number(res.data.totalResults)) break;
-              
-              list.push(i);
-            }
+      let list = [];
 
-            if(!res.data.totalResults) list = [];
-            console.log('state')
-            this.setState({
-              results: res.data.Search,
-              totalMovies: Number(res.data.totalResults),
-              search: searchTerm,
-              pageList: list,
-              page: page
-            })
-          })
-          .catch(err => {
-            console.log(err);
-          })
+      for(let i = 1; i <= pageListEnd; i++ ) {
+        if(i > Number(res.data.totalResults)) break;
+        
+        list.push(i);
+      }
+
+      if(!res.data.totalResults) list = [];
+
+      this.setState({
+        results: res.data.Search,
+        totalMovies: Number(res.data.totalResults),
+        search: searchTerm,
+        pageList: list,
+        page: res.config.params.newpage
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
@@ -90,7 +89,7 @@ class App extends Component {
 
 
         <div className='page-index'>
-            <span> &#x2039; </span>
+            <span onClick={this.decrementPage}> &#x2039; </span>
 
             <ul>
               {this.state.pageList.map(pageNum => <li key={pageNum} onClick={this.selectPage}>{pageNum}</li>)}
