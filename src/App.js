49c52d19748de714b48b5144e05fb1f4c75e87ed
Trip4077 <bj2336@email.vccs.edu>
2019-03-13 18:45:27 -0400

    Search View and Route Implemented

--- src/App.js
+++ src/App.js
@@ -1,88 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
 
-import Search from './components/Searchbar/Search';
-import PageSelect from './components/Pagination/PageSelect';
-import Thumbnail from './components/Movies/Thumbnail';
+import SearchPage from './views/SearchPage';
+
+import { Route } from 'react-router-dom';
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      search: '',
-      plot: 'short',
-      page: 1,
-      pageList: [],
-      totalMovies: 0,
-      results: []
-    }
-  }
-
-  handleChange = e => {
-    this.setState({ search: e.target.value });
-  }
-
-  performSearch = (e, newpage) => {
-    e.preventDefault();
-
-    const page = newpage ? newpage : this.state.page;
-    const lastPage = this.state.pageList[this.state.pageList.length-1]
-    const searchTerm = this.state.search;
-
-    if (page > lastPage || page < 1) return;
-    
-    axios.get(`http://www.omdbapi.com/?s=${searchTerm}&plot=${this.state.plot}&page=${page}&apikey=db6f6716`, {
-      params: { newpage: page }
-    })
-    .then(res => {
-      const pageListEnd = res.data.totalResults % 10 === 0 ? Math.floor(res.data.totalResults / 10)
-                                                      : Math.floor(res.data.totalResults / 10) + 1;
-
-      let list = [];
-
-      for(let i = 1; i <= pageListEnd; i++ ) {
-        if(i > Number(res.data.totalResults)) break;
-        
-        list.push(i);
-      }
-
-      if(!res.data.totalResults) list = [];
-
-      this.setState({
-        results: res.data.Search,
-        totalMovies: Number(res.data.totalResults),
-        search: searchTerm,
-        pageList: list,
-        page: res.config.params.newpage
-      })
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-
   render() {
-    console.log(this.state.results)
     return (
       <div className="App">
-
-        <Search handleChange={this.handleChange}
-                performSearch={this.performSearch}
-                search={this.state.search}
-                />
-
-        <div>
-          {this.state.results && this.state.results.length > 0 
-                              ? this.state.results.map(movie => <Thumbnail key={Math.random()} movie={movie} />)
-                              : <p>No Results Found</p>}
-        </div>
-
-
-        <PageSelect performSearch={this.performSearch}
-                    page={this.state.page}
-                    pageList={this.state.pageList}/>
+        <Route path='/search' component={SearchPage} />
       </div>
     );
   }
